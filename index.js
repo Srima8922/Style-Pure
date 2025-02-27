f92dd0698b87e27012568c49de7ec158ed484bdd
Srima8922 <srimathis8922@gmail.com>
2025-02-27 19:14:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var sidenav = document.querySelector(".side-navbar")
+
+function showNavbar() {
+    sidenav.style.left = "0"
+    sidenav.style.display = "block"
+
+}
+function closeNavbar() {
+    sidenav.style.left = "-60%"
+}
